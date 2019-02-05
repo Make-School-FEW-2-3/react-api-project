a0b1efa4cd27d72c7965f9cccd5a422c50326cf6
Severiano Badajoz <sevebadajoz@gmail.com>
2019-02-04 16:51:20 -0800

    Add Temperature Component

---
+++ src/Temperature.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Temperature extends Component {
+  kToF = (temp) => {
+    const farenheit = (this.props.temp - 273.15) * 9/5 + 32;
+    return farenheit.toPrecision(3);
+  }
+  
+  render() {
+    return (
+        <div>Temp: {this.kToF()}</div>
+    )
+  }
+}
--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Temperature from './Temperature';
 
 export default class WeatherData extends Component {
   render() {
@@ -7,7 +8,7 @@ export default class WeatherData extends Component {
           <div>Title: {this.props.main}</div>
           <div>Desc: {this.props.description}</div>
           <div>Icon: {this.props.icon}</div>
-          <div>Temp: {this.props.temp}</div>
+          <Temperature temp={this.props.temp}/>
           <div>Pressure: {this.props.pressure}</div>
           <div>Humidity: {this.props.humidity}</div>
           <div>Temp Min: {this.props.temp_min} Max:{this.props.temp_max}</div>
